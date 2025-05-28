2f388fc65f15e45da85ea9e23243bf9161df6099
zen4xx <yarikshcher@gmail.com>
2025-05-29 01:16:10 +0300

    updated architecture

--- window/window.cpp
+++ window/window.cpp
@@ -1,8 +1,6 @@
 #include "window.h"
 
 Window::Window(int width, int height, const char* name, GLFWmonitor* monitor, GLFWwindow* share){
-    
-    glfwInit();
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     m_window = glfwCreateWindow(width, height, name, monitor, share);
