f3aff351f6cccc7eeeaedc2d203f0ee38fc57899
zen4xx <yarikshcher@gmail.com>
2025-07-11 00:05:14 +0300

    added update scene func part 1.5

--- scene/scene.cpp
+++ scene/scene.cpp
@@ -1,2 +1,56 @@
 #include "scene.h"
 
+void _Scene::createDescriptorSetsForScene(VkExtent2D extent, VmaAllocator allocator, VkDescriptorSetLayout descriptor_set_layout, VkDevice device)
+{
+    proj = glm::perspective(glm::radians(45.0f), extent.width / (float)extent.height, 0.1f, 10.0f);
+    createDescriptorPool(&descriptor_pool, objects.size(), allocator, device);
+    createDescriptorSets(descriptor_sets, descriptor_set_layout, objects.size(), descriptor_pool, device);
+    int i = 0;
+    for (auto it = objects.begin(); it != objects.end(); ++it)
+    {
+        addDescriptorSet(descriptor_sets[i], it->second->uniformBuffer, device);
+        it->second->descriptorSet = &descriptor_sets[i];
+        ++i;
+    }
+    isDescriptorSetsCreated = 1;
+}
+
+void _Scene::destroyDescriptorPool(VmaAllocator allocator, VkDevice device)
+{
+    vkDestroyDescriptorPool(device, descriptor_pool, nullptr);
+}
+
+void _Scene::deleteScene(VmaAllocator allocator, VkDevice device)
+{
+    for (auto it = objects.begin(); it != objects.end(); ++it)
+    {
+        auto& obj = it->second;
+
+        if (obj->uniformBufferMemory != VK_NULL_HANDLE && obj->uniformBufferMapped != nullptr)
+        {
+            vmaUnmapMemory(allocator, obj->uniformBufferMemory); 
+            obj->uniformBufferMapped = nullptr;
+        }
+
+        if (obj->vertexBuffer != VK_NULL_HANDLE)
+        {
+            vmaDestroyBuffer(allocator, obj->vertexBuffer, obj->vertexBufferMemory);
+        }
+
+        if (obj->indexBuffer != VK_NULL_HANDLE)
+        {
+            vmaDestroyBuffer(allocator, obj->indexBuffer, obj->indexBufferMemory);
+        }
+
+        if (obj->uniformBuffer != VK_NULL_HANDLE)
+        {
+            vmaDestroyBuffer(allocator, obj->uniformBuffer, obj->uniformBufferMemory);
+        }
+    }
+
+    if (descriptor_pool != VK_NULL_HANDLE)
+    {
+        vkDestroyDescriptorPool(device, descriptor_pool, nullptr);
+        descriptor_pool = VK_NULL_HANDLE;
+    }
+}
--- scene/scene.h
+++ scene/scene.h
@@ -13,38 +13,10 @@
 class _Scene
 {
 public:
-    void createDescriptorSetsForScene(VkExtent2D extent, VmaAllocator allocator, VkDescriptorSetLayout descriptor_set_layout, VkDevice device)
-    {
-        proj = glm::perspective(glm::radians(45.0f), extent.width / (float)extent.height, 0.1f, 10.0f);
-        createDescriptorPool(&descriptor_pool, objects.size(), allocator, device);
-        createDescriptorSets(descriptor_sets, descriptor_set_layout, objects.size(), descriptor_pool, device);
-        int i = 0;
-        for (auto it = objects.begin(); it != objects.end(); ++it)
-        {
-            addDescriptorSet(descriptor_sets[i], it->second->uniformBuffer, device);
-            it->second->descriptorSet = &descriptor_sets[i];
-            ++i;
-        }
-        isDescriptorSetsCreated = 1;
-    }
+    void createDescriptorSetsForScene(VkExtent2D extent, VmaAllocator allocator, VkDescriptorSetLayout descriptor_set_layout, VkDevice device);
 
-    void destroyDescriptorPool(VmaAllocator allocator, VkDevice device)
-    {
-        vkDestroyDescriptorPool(device, descriptor_pool, nullptr);
-    }
-    
-    void deleteScene(VmaAllocator allocator, VkDevice device)
-    {
-        for (auto it = objects.begin(); it != objects.end(); ++it)
-        {
-            vmaDestroyBuffer(allocator, it->second->vertexBuffer, it->second->vertexBufferMemory);
-            vmaDestroyBuffer(allocator, it->second->indexBuffer, it->second->indexBufferMemory);
-            vmaUnmapMemory(allocator, it->second->uniformBufferMemory);
-            vmaDestroyBuffer(allocator, it->second->uniformBuffer, it->second->uniformBufferMemory);
-        }
-        destroyDescriptorPool(allocator, device);
-    }
-    
+    void destroyDescriptorPool(VmaAllocator allocator, VkDevice device);
+    void deleteScene(VmaAllocator allocator, VkDevice device);
 
 public:
     bool isDescriptorSetsCreated = 0;
